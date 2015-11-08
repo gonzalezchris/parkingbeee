0ceb02d8ac7a50656682e65321e6146a861794e8
MTSS User <mtss@Macintosh.local>
2015-11-08 13:06:03 -0500

    Initial Commit

---
+++ ParkingBee/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ParkingBee
+//
+//  Created by MTSS User on 11/8/15.
+//  Copyright (c) 2015 MTSS User. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
